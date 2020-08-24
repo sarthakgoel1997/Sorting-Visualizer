03a89a875e995bcf4b650cafbbb751fda93e83ea
Sarthak Goel <69145032+sarthakgoel97@users.noreply.github.com>
2020-08-24 20:48:06 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SortingVisualiser from './SortingVisualiser/SortingVisualiser';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualiser></SortingVisualiser>
+    </div>
+  );
+}
+
+export default App;
